146cbabdcae70cab9649ade36281d314ab5a2317
rcerc <88944439+rcerc@users.noreply.github.com>
2025-08-03 12:29:15 -0400

    modules/etc: Support timezone passthrough or override
    This needs more work; see `TODO`.

--- modules/etc.nix
+++ modules/etc.nix
@@ -1,14 +1,33 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
-  cfg = config.etc.sslCertificates;
+  # Adapted from `nixpkgs/nixos/modules/config/locale.nix`
+  nospace = str: lib.filter (c: c == " ") (lib.stringToCharacters str) == [ ];
+  tzOverrideType = lib.types.nullOr (lib.types.addCheck lib.types.str nospace) // {
+    description = "null or string without spaces";
+  };
 in
 
 with lib;
 
 {
-  options = {
-    etc.sslCertificates = {
+  options.etc = {
+    timezoneOverride = mkOption {
+      default = "UTC";
+      type = tzOverrideType;
+      example = "America/New_York";
+      description = ''
+        The timezone in the sandbox.  If null, the system timezone will be
+        passed through.
+      '';
+    };
+
+    sslCertificates = {
       enable = mkEnableOption "SSL/TLS certificate support";
       path = mkOption {
         description = "SSL/TLS certificate bundle file";
@@ -18,10 +37,40 @@ with lib;
     };
   };
 
-  config = mkIf cfg.enable {
-    bubblewrap.bind.ro = [
-      [ cfg.path "/etc/ssl/certs/ca-bundle.crt" ]
-      [ cfg.path "/etc/ssl/certs/ca-certificates.crt" ]
-    ];
-  };
+  config.bubblewrap = mkMerge [
+    (
+      let
+        tz = config.etc.timezoneOverride;
+      in
+      mkIf (tz != "UTC") {
+        # TODO: According to LOCALTIME(5), `/etc/localtime` should be a symlink
+        # into `/etc/zoneinfo/` so that it provides the timezone name.  However
+        # bind mount destinations are resolved.  We should read the destination
+        # of `/etc/localtime` and recreate the symlink in the sandbox.
+        bind.ro = [ "/etc/zoneinfo" ] ++ (lib.optional (tz == null) "/etc/localtime");
+        symlinks = lib.optional (tz != null) [
+          "/etc/localtime"
+          "/etc/zoneinfo/${tz}"
+        ];
+      }
+    )
+
+    (
+      let
+        cfg = config.etc.sslCertificates;
+      in
+      mkIf cfg.enable {
+        bind.ro = [
+          [
+            cfg.path
+            "/etc/ssl/certs/ca-bundle.crt"
+          ]
+          [
+            cfg.path
+            "/etc/ssl/certs/ca-certificates.crt"
+          ]
+        ];
+      }
+    )
+  ];
 }
